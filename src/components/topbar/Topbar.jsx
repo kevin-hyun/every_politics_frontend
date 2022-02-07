b6056a1bfac1697429c118d9dba707dba7c9deaa
hyubee <hyubee@hyubees-MacBook-Pro.local>
2022-02-07 16:23:33 +0900

    Feat : topbar 기본 레이아웃

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./topbar.css";
+
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">국민의 일꾼</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            alt="profile"
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
